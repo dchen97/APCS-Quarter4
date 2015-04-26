735247ae7a5f0478663ea2200abba09a86eccd0f
Diana <dchen@imsa.edu>
2015-04-26 14:33:54 -0500

    Add Java files and copy Magpie Lab

---
+++ Magpie Lab + GUI/GUI.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class GUI here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GUI
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class GUI
+     */
+    public GUI()
+    {
+        
+    }
+    
+}
