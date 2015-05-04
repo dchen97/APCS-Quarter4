f96a7e36f6b2d41f9042cdf00b502ca5102763a4
Diana <dchen@imsa.edu>
2015-05-04 08:26:16 -0500

    Adding initial variables and setting up the panel

--- Magpie Lab + GUI/GUI.java
+++ Magpie Lab + GUI/GUI.java
@@ -1,19 +1,42 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Dimension;
 /**
  * Write a description of class GUI here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Diana Chen
+ * @5/8/15
  */
 public class GUI
 {
     // instance variables - replace the example below with your own
-
+	private JPanel p;
+	private JButton sendButt;
+	private JButton restartButt;
+	private JLabel userInputLabel;
+	private JLabel chatOutputLabel;
     /**
      * Constructor for objects of class GUI
      */
     public GUI()
     {
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Chatbox");
+		p = new JPanel();
+		sendButt = new JButton();
+		sendButt.setText("Send");
+		restartButt = new JButton();
+		restartButt.setText("New Conversation");
+		userInputLabel = new JLabel("You:");
+		JTextField userInput = new JTextField(200);
+		chatOutputLabel = new JLabel("Chatbot: ");
+		JTextField chatOutput = new JTextField(200);
+		ImageIcon chatPic = new ImageIcon(getClass().getResource(
+		JLabel chatPic = new JLabel();
+		p.add(sendButt);
+		
+		
         
     }
     
